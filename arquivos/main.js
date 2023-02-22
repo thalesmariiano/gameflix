4b1d45919b6eda7a182977ac7438687dcd84389e
Thales Mariano <thalesmariano124@gmail.com>
2023-02-22 17:21:52 -0300

    game info modal added - in work

--- arquivos/main.js
+++ arquivos/main.js
@@ -2,7 +2,21 @@
 const KEY = "efb519ffa3e047cebdba546fdcfd63d2"
 const url = `https://api.rawg.io/api/games?key=${KEY}`
 
+const body = document.querySelector("body")
+
+const modal_container = document.querySelector("#modal-container")
+const modal = document.querySelector("#modal")
+const close_modal = document.querySelector("#close-modal")
+const modal_game_platforms = document.querySelector("#game-platforms")
+const modal_game_name = document.querySelector("#game-name")
+const modal_game_background = document.querySelector("#modal-game-background")
+const modal_game_release_date = document.querySelector("#game-release-date")
+const modal_game_rating = document.querySelector("#game-rating")
+const modal_game_description = document.querySelector("#game-description")
+
 const games_carrousel  = document.querySelectorAll(".games-carrousel")
+const games  = document.querySelectorAll(".games")
+
 const popular_games    = document.querySelectorAll('[data-games="popular"]')
 const play_again_games = document.querySelectorAll('[data-games="play-again"]')
 const action_games     = document.querySelectorAll('[data-games="action"]')
@@ -19,6 +33,50 @@ games_carrousel.forEach(carrousel => {
 	  direction: 'horizontal'
 	});
 })
+
+close_modal.addEventListener("click", () => {
+	modal_container.classList.add("hidden")
+	modal_container.classList.remove("flex")
+	body.classList.add("overflow-auto")
+	body.classList.remove("overflow-hidden")
+})
+
+games.forEach(game => {
+	game.addEventListener("click", () => {
+		if(game.dataset.name){
+			const d_div = games_array.find(g => g.name == game.dataset.name)
+			modal_container.classList.add("flex")
+			modal_container.classList.remove("hidden")
+			body.classList.add("overflow-hidden")
+			body.classList.remove("overflow-auto")
+
+			modal_game_name.innerHTML = d_div.name
+			modal_game_platforms.innerHTML = ""
+			d_div.platforms.forEach(p => {
+				modal_game_platforms.innerHTML += `<p class="mx-1">${p.platform.name}</p>`
+			})
+			modal_game_background.src = d_div.short_screenshots[1].image
+			const date = new Date(d_div.released)
+			modal_game_release_date.innerHTML = date.getFullYear()
+			modal_game_rating.innerHTML = d_div.esrb_rating ? d_div.esrb_rating.name : "Não classificado"
+			modal_game_rating.style.background = "#a18257"
+			switch(d_div.esrb_rating.id){
+				case 1:
+				case 2:
+					modal_game_rating.style.background = "rgb(22 101 52)"
+					break
+				case 3:
+					modal_game_rating.style.background = "rgb(133 77 14)"
+					break
+				case 4:
+					modal_game_rating.style.background = "rgb(153 27 27)"
+					break
+			}
+		}
+	})
+})
+
+const games_array = []
 
 search_btn.addEventListener("click", () => {
 	search_input.focus()
@@ -42,7 +100,9 @@ async function setGames(carrousel_item, genre, count){
 	const games = await getGames(genre, count)
 
 	for(i = 0; i < carrousel_item.length; i++){
+		carrousel_item[i].dataset.name = `${games[i].name}`
 		carrousel_item[i].children[1].innerHTML = `<img src="${games[i].background_image}" alt="${games[i].name}">`
+		games_array.push(games[i])
 	}
 }
 
@@ -76,23 +136,23 @@ function scrollTrigger({selector, offeset = 0, execute, viewTrigger}){
 
 setGames(popular_games, "", 6)
 
-scrollTrigger({
-	selector: "#play-again-carrousel",
-	offeset: 700,
-	execute: () => setGames(play_again_games, "indie", 6),
-	// viewTrigger: true
-})
+// scrollTrigger({
+// 	selector: "#play-again-carrousel",
+// 	offeset: 700,
+// 	execute: () => setGames(play_again_games, "indie", 6),
+// 	// viewTrigger: true
+// })
 
-scrollTrigger({
-	selector: "#action-carrousel",
-	offeset: 600,
-	execute: () => setGames(action_games, "action", 6),
-	// viewTrigger: true
-})
+// scrollTrigger({
+// 	selector: "#action-carrousel",
+// 	offeset: 600,
+// 	execute: () => setGames(action_games, "action", 6),
+// 	// viewTrigger: true
+// })
 
-scrollTrigger({
-	selector: "#adventure-carrousel",
-	offeset: 500,
-	execute: () => setGames(adventure_games, "adventure", 6),
-	// viewTrigger: true
-})
+// scrollTrigger({
+// 	selector: "#adventure-carrousel",
+// 	offeset: 500,
+// 	execute: () => setGames(adventure_games, "adventure", 6),
+// 	// viewTrigger: true
+// })
