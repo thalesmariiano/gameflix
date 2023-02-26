7ffc136ad9fd86d1526d60bfc34cfc067d8b57f2
Thales Mariano <thalesmariano124@gmail.com>
2023-02-25 22:48:12 -0300

    Some refactors in fetch API

--- arquivos/main.js
+++ arquivos/main.js
@@ -17,7 +17,7 @@ const modal_game_description = document.querySelector("#game-description")
 const modal_game_genres = document.querySelector("#game-genres")
 
 const games_carrousel  = document.querySelectorAll(".games-carrousel")
-const games  = document.querySelectorAll(".games")
+const games = document.querySelectorAll(".games")
 
 const popular_games    = document.querySelectorAll('[data-games="popular"]')
 const play_again_games = document.querySelectorAll('[data-games="play-again"]')
@@ -41,84 +41,108 @@ close_modal.addEventListener("click", () => {
 	modal_container.classList.remove("flex")
 	body.classList.add("overflow-auto")
 	body.classList.remove("overflow-hidden")
-})
 
-games.forEach(game => {
-	game.addEventListener("click", () => {
-		if(game.dataset.name){
-			const d_div = games_array.find(g => g.name == game.dataset.name)
-			modal_container.classList.add("flex")
-			modal_container.classList.remove("hidden")
-			body.classList.add("overflow-hidden")
-			body.classList.remove("overflow-auto")
-
-			modal_game_name.innerHTML = d_div.name
-			modal_game_platforms.innerHTML = ""
-			d_div.platforms.forEach(p => {
-				switch(p.platform.id){
-					case 1:
-					case 14:
-						modal_game_platforms.innerHTML += `<i class="fa-brands fa-xbox text-white mx-1"></i>`
-						break
-					case 3:
-					case 5:
-						modal_game_platforms.innerHTML += `<i class="fa-brands fa-apple text-white mx-1"></i>`
-						break
-					case 4:
-						modal_game_platforms.innerHTML += `<p class="text-white mx-1">PC</p>`
-						break
-					case 6:
-						modal_game_platforms.innerHTML += `<i class="fa-brands fa-linux text-white mx-1"></i>`
-						break
-					case 16:
-					case 18:
-					case 19:
-						modal_game_platforms.innerHTML += `<i class="fa-brands fa-playstation text-white mx-1"></i>`
-						break
-					case 21:
-						modal_game_platforms.innerHTML += `<i class="fa-brands fa-android text-white mx-1"></i>`
-						break
-
-				}
-			})
-			modal_game_background.src = d_div.short_screenshots[1].image
-			const date = new Date(d_div.released)
-			modal_game_release_date.innerHTML = date.getFullYear()
-			modal_game_rating.innerHTML = d_div.esrb_rating ? d_div.esrb_rating.name : "Não classificado"
-			modal_game_rating.style.background = "#a18257"
-			switch(d_div.esrb_rating.id){
-				case 1:
-				case 2:
-					modal_game_rating.style.background = "rgb(22 101 52)"
-					break
-				case 3:
-					modal_game_rating.style.background = "rgb(133 77 14)"
-					break
-				case 4:
-					modal_game_rating.style.background = "rgb(153 27 27)"
-					break
-			}
-			const maxRating = d_div.ratings.reduce((prev, current) => prev.count > current.count ? prev : current)
-			modal_game_public_rating.innerHTML = maxRating ? maxRating.title : "Sem avaliações"
-			modal_game_public_rating.style.color = "white"
-			switch(maxRating.id){
-				case 1:
-				case 3:
-					modal_game_public_rating.style.color = "#ba6829"
-					break
-				case 2:
-				case 4:
-				case 5:
-					modal_game_public_rating.style.color = "#0b9616"
-					break
-			}
-			modal_game_genres.innerHTML = ""
-			d_div.genres.forEach(genre => modal_game_genres.innerHTML += `${genre.name}, `)
-		}
-	})
+	modal_game_platforms.innerHTML = ""
+	modal_game_background.src = ""
+	modal_game_genres.innerHTML = ""
 })
 
 const games_array = []
+
+const getGames = async (genre, count) => {
+	const genres = genre ? `&genres=${genre}&` : "&"
+	const gamesFetched = await fetch(`${url}${genres}page_size=${count}&page=3`).then(response => response.json())
+						.then(data => data.results)
+	for(index in gamesFetched){
+		games_array.push(gamesFetched[index])		
+	}
+	return gamesFetched
+}
+
+const setGames = async (carrousel_item, genre, count) => {
+	const games = await getGames(genre, count)
+
+	for(i = 0; i < carrousel_item.length; i++){
+		const {name, background_image} = games[i]
+
+		carrousel_item[i].dataset.name = name
+		carrousel_item[i].children[1].innerHTML = `<img src="${background_image}" alt="${name}">`
+		carrousel_item[i].addEventListener("click", setModalInfo)
+	}
+}
+
+function setModalInfo(e){
+	modal_container.classList.add("flex")
+	modal_container.classList.remove("hidden")
+	body.classList.add("overflow-hidden")
+	body.classList.remove("overflow-auto")
+
+	const { name, platforms, esrb_rating, ratings, genres, short_screenshots, released } = games_array.find(game => game.name == e.target.offsetParent.dataset.name)
+	modal_game_name.innerHTML = name
+	platforms.forEach(p => {
+		const { platform } = p
+		switch(platform.id){
+			case 1:
+			case 14:
+				modal_game_platforms.innerHTML += `<i class="fa-brands fa-xbox text-white mx-1"></i>`
+				break
+			case 3:
+			case 5:
+				modal_game_platforms.innerHTML += `<i class="fa-brands fa-apple text-white mx-1"></i>`
+				break
+			case 4:
+				modal_game_platforms.innerHTML += `<i class="fa-brands fa-windows text-white mx-1"></i>`
+				break
+			case 6:
+				modal_game_platforms.innerHTML += `<i class="fa-brands fa-linux text-white mx-1"></i>`
+				break
+			case 16:
+			case 18:
+			case 19:
+				modal_game_platforms.innerHTML += `<i class="fa-brands fa-playstation text-white mx-1"></i>`
+				break
+			case 21:
+				modal_game_platforms.innerHTML += `<i class="fa-brands fa-android text-white mx-1"></i>`
+				break
+		}
+	})
+
+	modal_game_background.src = short_screenshots[2].image
+
+	const gameReleaseDate = new Date(released).getFullYear()
+	modal_game_release_date.innerHTML = gameReleaseDate
+
+	modal_game_rating.innerHTML = esrb_rating ? esrb_rating.name : "Não classificado"
+	modal_game_rating.style.background = "#a18257"
+	switch(esrb_rating.id){
+		case 1:
+		case 2:
+			modal_game_rating.style.background = "rgb(22 101 52)"
+			break
+		case 3:
+			modal_game_rating.style.background = "rgb(133 77 14)"
+			break
+		case 4:
+			modal_game_rating.style.background = "rgb(153 27 27)"
+			break
+	}
+
+	const maxRating = ratings.reduce((prev, current) => prev.count > current.count ? prev : current)
+	modal_game_public_rating.innerHTML = maxRating ? maxRating.title : "Sem avaliações"
+	modal_game_public_rating.style.color = "white"
+	switch(maxRating.id){
+		case 1:
+		case 3:
+			modal_game_public_rating.style.color = "#ba6829"
+			break
+		case 2:
+		case 4:
+		case 5:
+			modal_game_public_rating.style.color = "#0b9616"
+			break
+	}
+	genres.forEach(genre => modal_game_genres.innerHTML += `${genre.name}, `)
+}
 
 search_btn.addEventListener("click", () => {
 	search_input.focus()
@@ -131,22 +155,6 @@ search_input.addEventListener("focusout", () => {
 	search_container.classList.remove("border-white", "w-72", "bg-neutral-700")
 })
 
-function getGames(genre, count){
-	const genres = genre ? `&genres=${genre}&` : "&"
-	return fetch(`${url}${genres}page_size=${count}&page=3`)
-			.then(response => response.json())
-				.then(data => data.results)
-}
-
-async function setGames(carrousel_item, genre, count){
-	const games = await getGames(genre, count)
-
-	for(i = 0; i < carrousel_item.length; i++){
-		carrousel_item[i].dataset.name = `${games[i].name}`
-		carrousel_item[i].children[1].innerHTML = `<img src="${games[i].background_image}" alt="${games[i].name}">`
-		games_array.push(games[i])
-	}
-}
 
 function scrollTrigger({selector, offeset = 0, execute, viewTrigger}){
 	const position = document.querySelector(selector).getBoundingClientRect()
@@ -178,23 +186,23 @@ function scrollTrigger({selector, offeset = 0, execute, viewTrigger}){
 
 setGames(popular_games, "", 6)
 
-// scrollTrigger({
-// 	selector: "#play-again-carrousel",
-// 	offeset: 700,
-// 	execute: () => setGames(play_again_games, "indie", 6),
-// 	// viewTrigger: true
-// })
+scrollTrigger({
+	selector: "#play-again-carrousel",
+	offeset: 700,
+	execute: () => setGames(play_again_games, "indie", 6),
+	// viewTrigger: true
+})
 
-// scrollTrigger({
-// 	selector: "#action-carrousel",
-// 	offeset: 600,
-// 	execute: () => setGames(action_games, "action", 6),
-// 	// viewTrigger: true
-// })
+scrollTrigger({
+	selector: "#action-carrousel",
+	offeset: 600,
+	execute: () => setGames(action_games, "action", 6),
+	// viewTrigger: true
+})
 
-// scrollTrigger({
-// 	selector: "#adventure-carrousel",
-// 	offeset: 500,
-// 	execute: () => setGames(adventure_games, "adventure", 6),
-// 	// viewTrigger: true
-// })
+scrollTrigger({
+	selector: "#adventure-carrousel",
+	offeset: 500,
+	execute: () => setGames(adventure_games, "adventure", 6),
+	// viewTrigger: true
+})
